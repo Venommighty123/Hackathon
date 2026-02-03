bf58410a1c0e60344ec28833070dd5bc7300d39e
Venommighty123 <mathurkushagra163@gmail.com>
2026-02-03 12:54:00 +0530

    Use environment variable for MongoDB connection URI
    Replaced hardcoded MongoDB URI with environment variable.

--- My Portion/app/transfer_employee.py
+++ My Portion/app/transfer_employee.py
@@ -9,7 +9,7 @@ from langchain_groq import ChatGroq
 from .schemas import AdminOutcome, TransferApplication
 from .retriever import MongoDBHRPipeline
 
-ATLAS_URI = "mongodb+srv://mathurkushagra163_db_user:ElTitz0clXuFXUeu@cluster0.1c2z3gd.mongodb.net/?appName=Cluster0"
+ATLAS_URI = os.getenv("MONGO_URI")
 
 # Async Mongo Client
 client = AsyncIOMotorClient(ATLAS_URI)
